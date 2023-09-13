fb48d314fc6210e98c15fa1e086c2969f46da5c2
jcruz-2018059 <jcruz-2018059@kinal.edu.gt>
2023-09-13 15:15:22 -0600

    Ejercicio semana 3

--- src/app/security/directives/copyicon.directive.ts
+++ src/app/security/directives/copyicon.directive.ts
@@ -1,32 +1,42 @@
 // Angular
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCopyicon]'
 })
 export class CopyiconDirective {
+  private copied = false;
+
   constructor(
     private el: ElementRef,
-    private renderer: Renderer2,
-    private snackBar: MatSnackBar
-  ) {
-    this.renderer.addClass(this.el.nativeElement, 'copyable');
-    this.renderer.setProperty(this.el.nativeElement, 'title', 'Click to copy');
-  }
+    private renderer: Renderer2
+  ) {}
 
   @HostListener('click')
   onClick() {
-    const textToCopy = this.el.nativeElement.textContent.trim();
+    if (!this.copied) {
+      const copyButton = this.renderer.createElement('button');
+      this.renderer.setProperty(copyButton, 'textContent', 'Copiar');
+      this.renderer.setAttribute(copyButton, 'class', 'btn btn-primary');
+      this.renderer.appendChild(this.el.nativeElement, copyButton);
+
+      const textToCopy = this.el.nativeElement.textContent;
+      this.copyTextToClipboard(textToCopy);
+      this.copied = true;
+
+      copyButton.addEventListener('click', () => {
+        this.renderer.removeChild(this.el.nativeElement, copyButton);
+        this.copied = false;
+      });
+    }
+  }
+
+  private copyTextToClipboard(text: string) {
     const textArea = document.createElement('textarea');
-    textArea.value = textToCopy;
+    textArea.value = text;
     document.body.appendChild(textArea);
     textArea.select();
     document.execCommand('copy');
     document.body.removeChild(textArea);
-
-    this.snackBar.open('Copied to clipboard', 'Close', {
-      duration: 2000
-    });
   }
 }
